689ba7874cfca2ed5daf0faa04191f926c9c5d70
Gyro <gamingskullgamer@gmail.com>
2022-12-16 08:33:35 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Spline from '@splinetool/react-spline';
+
+function App() {
+
+  return (
+    <div>
+      <Spline scene="https://prod.spline.design/fftP6Be4P3pN7LFX/scene.splinecode" />
+    </div>
+  )
+}
+
+export default App
